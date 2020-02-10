c886acc33b58db4d938ae9c1bab4e3c580f9d516
Jean Carlo <jean@codebit.com.br>
2020-02-10 16:38:07 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/api/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.api.cursomc.repositories;
+
+import com.api.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Serializable> {
+}
