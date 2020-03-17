1d93c2bbb27fe722be44df40599ccc6eef6a99f0
Jean Carlo <jean@codebit.com.br>
2020-03-17 20:25:46 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/api/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.api.cursomc.repositories;
+
+import com.api.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
