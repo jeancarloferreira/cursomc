bce66f16e42588b549884119135ddead40491831
Jean Carlo <jean@codebit.com.br>
2020-04-07 21:31:54 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/api/cursomc/domain/enums/TipoCliente.java
+++ src/main/java/com/api/cursomc/domain/enums/TipoCliente.java
@@ -2,7 +2,7 @@ package com.api.cursomc.domain.enums;
 
 public enum TipoCliente {
     PESSOAFISICA(1, "Pessoa Física"),
-    PESSOAJURIDICA(1, "Pessoa Jurídica");
+    PESSOAJURIDICA(2, "Pessoa Jurídica");
 
     private int cod;
     private String descricao;
