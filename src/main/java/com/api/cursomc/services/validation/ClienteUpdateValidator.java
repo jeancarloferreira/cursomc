f7498f79d83d7b6fc33b02a464905b7d6f351a96
Jean Carlo <jean@codebit.com.br>
2020-04-09 16:18:33 -0300

    validacao customizada: email não repetido na atualizacao de Cliente

---
+++ src/main/java/com/api/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,51 @@
+package com.api.cursomc.services.validation;
+
+import com.api.cursomc.domain.Cliente;
+import com.api.cursomc.domain.enums.TipoCliente;
+import com.api.cursomc.dto.ClienteDTO;
+import com.api.cursomc.dto.ClienteNewDTO;
+import com.api.cursomc.repositories.ClienteRepository;
+import com.api.cursomc.resources.exception.FieldMessage;
+import com.api.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public void initialize(ClienteUpdate ann) {
+    }
+    @Override
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.parseInt(map.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null && !aux.getId().equals(uriId)) {
+            list.add(new FieldMessage("email", "Email já existente"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
