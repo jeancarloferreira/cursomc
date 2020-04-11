36cda09cb3e4908839e04f42e5c67e83c10eaae0
Jean Carlo <jean@codebit.com.br>
2020-04-11 15:50:14 -0300

    Busca de pedidos por nome e categorias

--- src/main/java/com/api/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/api/cursomc/repositories/ProdutoRepository.java
@@ -1,9 +1,20 @@
 package com.api.cursomc.repositories;
 
+import com.api.cursomc.domain.Categoria;
 import com.api.cursomc.domain.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Serializable> {
+
+    @Transactional(readOnly=true)
+    @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+    Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/api/cursomc/services/ProdutoService.java
@@ -0,0 +1,41 @@
+package com.api.cursomc.services;
+
+import com.api.cursomc.domain.Categoria;
+import com.api.cursomc.domain.Pedido;
+import com.api.cursomc.domain.Produto;
+import com.api.cursomc.repositories.CategoriaRepository;
+import com.api.cursomc.repositories.PedidoRepository;
+import com.api.cursomc.repositories.ProdutoRepository;
+import com.api.cursomc.services.exceptions.ObjectNotFoundException;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repo;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Produto find(Integer id){
+        Optional<Produto> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+    }
+
+    public Page<Produto> search(String nome, List<Serializable> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        List<Categoria> categorias = categoriaRepository.findAllById(ids);
+        return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+    }
+}
